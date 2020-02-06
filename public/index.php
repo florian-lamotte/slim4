90a9d68ea84608d597b91a719d0940159dba5701
florian-lamotte <fl.lamotte@gmail.com>
2020-02-06 20:08:44 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Add middleware
+$app->addRoutingMiddleware();
+
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->add(new BasePathMiddleware($app));
+
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
